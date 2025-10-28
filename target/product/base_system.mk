msft-mirror-aosp/platform.build

commit 4e019e59915c9955cf9b5e06052a06cd307a481d

Install WebApp APEX module when RELEASE_WEBAPP_MODULE is enabled
Bug: 453911870
Flag: build.RELEASE_WEBAPP_MODULE
Test: Presubmit
Change-Id: I271f0f06e36852bd9dbec3aa4981824d9bf36cbb

target/product/base_system.mk
@@ -402,6 +402,11 @@ ifeq ($(RELEASE_NPUMANAGER_MODULE),true)
        com.android.npumanager
 endif
 
+ifeq ($(RELEASE_WEBAPP_MODULE),true)
+    PRODUCT_PACKAGES += \
+       com.android.webapp
+endif
+
 ifneq (,$(RELEASE_RANGING_STACK))
     PRODUCT_PACKAGES += \
         com.android.ranging

commit cb025501e57f9a85a0b09ff62dd71955e8645431

Reland "Install WebApp APEX module when RELEASE_WEBAPP_MODULE is enabled"
This reverts commit 13554486a218e7a1e72a74e6effb043d48b0e9cc.

The previous commit 4e019e59915c9955cf9b5e06052a06cd307a481d broke GSI
and AOSP builds due to missing WebApp APEX module, which got reverted.
The issue should be fixed now.

Bug: 453911870
Flag: build.RELEASE_WEBAPP_MODULE
Test: lunch gsi_arm64-trunk_staging-userdebug && m
Change-Id: I1f9d008ca0ae8975bdddbdfe55258cffcb7f2059

target/product/base_system.mk
@@ -402,6 +402,11 @@ ifeq ($(RELEASE_NPUMANAGER_MODULE),true)
        com.android.npumanager
 endif
 
+ifeq ($(RELEASE_WEBAPP_MODULE),true)
+    PRODUCT_PACKAGES += \
+       com.android.webapp
+endif
+
 ifneq (,$(RELEASE_RANGING_STACK))
     PRODUCT_PACKAGES += \
         com.android.ranging