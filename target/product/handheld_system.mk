2d226c197e72f244a9cb53e396253c1ed0140b2d
sungcheolahn <donaldahn@google.com>
2026-02-12 03:00:49 +0000

    Install TelecomServiceResources on handheld systems.
    Test: atest TelecomUnitTests, CtsTelecomTestCases
    Bug: 450089987
    Flag: EXEMPT PURE_REFACTOR
    
    Change-Id: I4cc9a7ca95094b6df73728542ff67a75b2f534f2

--- target/product/handheld_system.mk
+++ target/product/handheld_system.mk
@@ -113,10 +113,12 @@ else
   PRODUCT_PACKAGES += \
       Telecom \
       TelecomUi \
+      TelecomServiceResources \
 
   PRODUCT_SYSTEM_SERVER_APPS += \
       Telecom \
       TelecomUi \
+      TelecomServiceResources \
 
 endif
 
