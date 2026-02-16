43d60a7e4e8d115cf1439c194e4ab025002e612d
Mårten Kongstad <amhk@google.com>
2026-02-16 11:38:24 +0000

    finalize-module-sdks: make --mainline-sdks-dir optional
    Make --mainline-sdks-dir optional. If --mainline-sdks-dir is not
    provided, build the module SDKs as part of finalize-module-sdks.
    
    This allows finalize-module-sdks to not depend on an official
    mainline_modules_sdks build, which in turn allows it to exit early if
    the module SDKs have already been finalized.
    
    Bug: 481594013
    Test: croot && ./build/tools/finalization/finalize-platform/main finalize-module-sdks
    Change-Id: Iffa9dc93f9a787d3a86cfa00435da4c36175fb6b

--- tools/finalization/finalize-platform/finalize-module-sdks.sh
+++ tools/finalization/finalize-platform/finalize-module-sdks.sh
@@ -19,6 +19,12 @@ if test -e "$TOP/prebuilts/sdk/extensions/$SDK_EXT_VERSION"; then
     return
 fi
 
+# The module SDKs do not exist already; build them
+if [[ -z "$mainline_sdks_dir" ]]; then
+    TARGET_RELEASE=sdk_finalization TARGET_BUILD_VARIANT=userdebug UNBUNDLED_BUILD_SDKS_FROM_SOURCE=true vendor/google/build/mainline_modules_sdks.sh --build-release next
+    mainline_sdks_dir="$DEST_DIR"
+fi
+
 m unpack-module-sdks
 projects="$(unpack-module-sdks \
     --mainline-sdks-top "$mainline_sdks_dir" \
