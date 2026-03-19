88c1a6a9e25afc5609ce1cb3417afd82fe750503
Bill Yang <yangbill@google.com>
2026-03-19 04:01:25 +0000

    Optimize 'enabled' attribute output in soong_api.json
    To reduce the JSON file content size, we recently updated the Soong
    logic to only emit the `enabled` attribute if it is false (ag/38985925).
    For Make modules, since the build system only collects modules that
    are actually enabled, we can safely remove the attribute directly.
    
    Bug: 493760053
    Test: m -j nothing, verify the `enabled` field in make-modules.json
    Change-Id: I7e7eb09736638b862cf8917b4e14ae050c8434eb

--- core/main.mk
+++ core/main.mk
@@ -1881,7 +1881,6 @@ define add-make-module-to-json
     $(call add_json_str, name, $(1)) \
     $(call add_json_str, type, $(sort $(ALL_MODULES.$(1).MAKE_MODULE_TYPE))) \
     $(call add_json_list, path, $(sort $(ALL_MODULES.$(1).PATH))) \
-    $(call add_json_bool, enabled, true) \
     $(if $(strip $(ALL_MODULES.$(1).INSTALLED)), \
       $(call add_json_list, install_files, $(sort $(ALL_MODULES.$(1).INSTALLED)))) \
     $(call add_json_bool, is_make_module, true) \
